39b36e968661b3e04fc4fa986dc496490bfa4f83
Kim-Jong-Gyu <whdrb2643@gmail.com>
2023-11-21 15:47:33 +0900

    first commit

---
+++ src/main/java/com/example/workerwholic/WorkerWholicApplication.java
@@ -0,0 +1,13 @@
+package com.example.workerwholic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkerWholicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkerWholicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/workerwholic/WorkerWholicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workerwholic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkerWholicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
