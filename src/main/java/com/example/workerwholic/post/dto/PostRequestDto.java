817e77ebffbe1d0f8519081448e49eb89e222448
Kim-Jong-Gyu <whdrb2643@gmail.com>
2023-11-24 21:53:48 +0900

    feat : Post CRUD 기능 구현

---
+++ src/main/java/com/example/workerwholic/post/dto/PostRequestDto.java
@@ -0,0 +1,11 @@
+package com.example.workerwholic.post.dto;
+
+import lombok.Getter;
+
+@Getter
+public class PostRequestDto {
+    private String title;
+    private String img;
+    private String content;
+
+}
